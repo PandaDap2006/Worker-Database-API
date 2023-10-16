87f3aea01fe0aaec9b632ca100dacca9b1f8b4d8
PandaDap2006 <Oliver.h.froberg@gmail.com>
2023-10-16 17:23:49 +0200

    Initial commit

---
+++ Backend Worker Database/Data/WorkerDatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Backend_Worker_Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend_Worker_Database.Data;
+
+public class WorkerDatabaseContext : DbContext {
+	public WorkerDatabaseContext(DbContextOptions<WorkerDatabaseContext> options) : base(options) {
+	}
+
+	public DbSet<Worker> Workers { get; set; } = null!;
+}
---
+++ Backend Worker Database/Models/Worker.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend_Worker_Database.Models;
+
+public class Worker {
+	public long id { get; set; }
+	public string profileImage { get; set; }
+	public string name { get; set; }
+	public string position { get; set; }
+}
